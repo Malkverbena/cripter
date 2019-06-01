6ba5b795603e2cf9a0e76fcc65eb7e9b4078d1f9
Cleber Alves <malkverbena@gmail.com>
2019-06-01 20:41:49 -0300

    Add files via upload
    Some cleanings.

--- cripter/cripter.cpp
+++ cripter/cripter.cpp
@@ -54,7 +54,9 @@ Variant cripter::decrypt_var_RSA(const PoolByteArray p_input, const String p_key
 //-------------- Simetric - GCM
 
 PoolByteArray cripter::encrypt_byte_GCM(const PoolByteArray p_input, const String p_key, const String p_add) const {
-
+	
+	int _err = 0;
+	
  	//Prepare key & iv **
 	String h_key = p_key.md5_text();
 	uint8_t key[KEY_SIZE];
@@ -67,8 +69,7 @@ PoolByteArray cripter::encrypt_byte_GCM(const PoolByteArray p_input, const Strin
 	}
 
 	//Preparing Buffer
-	int err = 0;
-    char erro[150];
+	char erro[150];
 	//Array ret;
 	uint8_t input[p_input.size()];
 	uint8_t output[sizeof(input)];
@@ -95,90 +96,104 @@ PoolByteArray cripter::encrypt_byte_GCM(const PoolByteArray p_input, const Strin
     mbedtls_gcm_setkey(&ctx, MBEDTLS_CIPHER_ID_AES, key, 256);
 
 	if (add_len == 0){
-		if( (err = mbedtls_gcm_crypt_and_tag(&ctx, MBEDTLS_GCM_ENCRYPT, sizeof(input), iv, EXT_SIZE, NULL, 0, input, output, TAG_SIZE, tag) ) != 0)
+		_err = mbedtls_gcm_crypt_and_tag(&ctx, MBEDTLS_GCM_ENCRYPT, sizeof(input), iv, EXT_SIZE, NULL, 0, input, output, TAG_SIZE, tag);
+		if( _err != 0)
 	{
-	    mbedtls_strerror( err, erro, sizeof(erro) );
+		mbedtls_strerror( _err, erro, sizeof(erro) );
 		print_error( erro );
-    }
+	}
 
-	}else{
-		if( (err = mbedtls_gcm_crypt_and_tag(&ctx, MBEDTLS_GCM_ENCRYPT, sizeof(input), iv, EXT_SIZE, add, add_len, input, output, TAG_SIZE, tag) ) != 0)
+	}else
 	{
-	    mbedtls_strerror( err, erro, sizeof(erro) );
-		print_error( erro );
-    }
+		_err = mbedtls_gcm_crypt_and_tag(&ctx, MBEDTLS_GCM_ENCRYPT, sizeof(input), iv, EXT_SIZE, add, add_len, input, output, TAG_SIZE, tag);
+		if( _err != 0)
+		{
+			mbedtls_strerror( _err, erro, sizeof(erro) );
+			print_error( erro );
+		}
 	}
 		
-    mbedtls_gcm_free( &ctx );
-       
-    PoolByteArray ret_output = char2pool(output, sizeof(output));
-   	PoolByteArray ret_tag = char2pool(tag, sizeof(tag));
-   	ret_output.append_array(ret_tag);
+	mbedtls_gcm_free( &ctx );
 
-  	return ret_output;
+	PoolByteArray ret_output = char2pool(output, sizeof(output));
+	PoolByteArray ret_tag = char2pool(tag, sizeof(tag));
+	ret_output.append_array(ret_tag);
+
+	return ret_output;
 
 }
 
 
 PoolByteArray cripter::decrypt_byte_GCM(const PoolByteArray p_input, const String p_key, const String p_add) const{
 	
+	int _err;
+	
 	//Prepare key & iv **
 	String h_key = p_key.md5_text();
 	uint8_t key[KEY_SIZE];
 	uint8_t iv[EXT_SIZE];
-	for (int i = 0; i < KEY_SIZE; i++) {
+	for (int i = 0; i < KEY_SIZE; i++) 
+	{
 		key[i] = h_key[i];
 	}
-	for (int i = 0; i < EXT_SIZE; i++){
-		iv[i] = key[i*2];	
+	for (int i = 0; i < EXT_SIZE; i++)
+	{
+		iv[i] = key[i*2];
 	}
 	
 	//Preparing Buffer
-	int err = 0;
-    char erro[150];
+
+	char erro[150];
 	PoolByteArray ret_output;
 	int data_len = p_input.size();
 	uint8_t input[(data_len - TAG_SIZE)];
 	uint8_t output[sizeof(input)];
 	
 	PoolVector<uint8_t>::Read r = p_input.read();   
-	for (int i = 0; i < (data_len - TAG_SIZE); i++) {
+	for (int i = 0; i < (data_len - TAG_SIZE); i++) 
+	{
 		input[i] = (uint8_t)p_input[i];
 	}
 				
 	//Extract Tag
 	uint8_t tag[TAG_SIZE];
 	PoolVector<uint8_t>::Read R = p_input.read();   
-	for (int i = 0; i < TAG_SIZE; i++) {
-		tag[i] = (uint8_t)p_input[ (data_len - TAG_SIZE) + i];
-	}
+	for (int i = 0; i < TAG_SIZE; i++) 
+		{
+			tag[i] = (uint8_t)p_input[ (data_len - TAG_SIZE) + i];
+		}
 		
 	//Prepare Addicional Data
 	int add_len = p_add.length();
 	uint8_t add[add_len];
-	for (int i = 0; i < add_len; i++) {
+	for (int i = 0; i < add_len; i++) 
+	{
 		add[i] = p_add[i];
 	}
 			
 	//Decryptation
 	mbedtls_gcm_context ctx;
 	mbedtls_gcm_init(&ctx);
-    mbedtls_gcm_setkey(&ctx, MBEDTLS_CIPHER_ID_AES, key, 256);
-     	
-	if (add_len == 0){
-		if( (err = mbedtls_gcm_auth_decrypt(&ctx, sizeof(input), iv, EXT_SIZE, NULL, 0, tag, TAG_SIZE, input, output) ) != 0)
-	{
-	    mbedtls_strerror( err, erro, sizeof(erro) );
-		print_error( erro );
-    }
+	mbedtls_gcm_setkey(&ctx, MBEDTLS_CIPHER_ID_AES, key, 256);
 
-	}else{
-		if( (err = mbedtls_gcm_auth_decrypt(&ctx, sizeof(input), iv, EXT_SIZE, add, add_len, tag, TAG_SIZE, input, output) ) != 0)
+	if (add_len == 0)
 	{
-	    mbedtls_strerror( err, erro, sizeof(erro) );
+		_err = mbedtls_gcm_auth_decrypt(&ctx, sizeof(input), iv, EXT_SIZE, NULL, 0, tag, TAG_SIZE, input, output);
+		if( _err != 0)
+		{
+			mbedtls_strerror( _err, erro, sizeof(erro) );
+			print_error( erro );
+		}
+
+	}else
+	{
+		_err = mbedtls_gcm_auth_decrypt(&ctx, sizeof(input), iv, EXT_SIZE, add, add_len, tag, TAG_SIZE, input, output);
+		if( _err != 0)
+	{
+	    mbedtls_strerror( _err, erro, sizeof(erro) );
 		print_error( erro );
-    }		
-    }
+	}		
+	}
 	    			
 	//Ending
 	mbedtls_gcm_free( &ctx );
@@ -189,15 +204,19 @@ PoolByteArray cripter::decrypt_byte_GCM(const PoolByteArray p_input, const Strin
 //-------------- Simetric - CBC
     
 PoolByteArray cripter::encrypt_byte_CBC(const PoolByteArray p_input, const String p_key) const{
-
+	
+	int _err;
+	
 	//Prepare key & iv **
 	String h_key = p_key.md5_text();
 	uint8_t key[KEY_SIZE];
 	uint8_t iv[EXT_SIZE];
-	for (int i = 0; i < KEY_SIZE; i++) {
+	for (int i = 0; i < KEY_SIZE; i++) 
+	{
 		key[i] = h_key[i];
 	}
-	for (int i = 0; i < EXT_SIZE; i++){
+	for (int i = 0; i < EXT_SIZE; i++)
+	{
 		iv[i] = key[i*2];	
 	}
 	
@@ -205,23 +224,25 @@ PoolByteArray cripter::encrypt_byte_CBC(const PoolByteArray p_input, const Strin
 	int data_len = p_input.size();
 	int extra_len;
 	int total_len;
-	int err = 0;
-    char erro[150];
+	char erro[150];
 
 	if (data_len % 16) { 
 		extra_len = (16 - (data_len % 16));    
 		total_len = data_len + extra_len ;   
-	} else { 
+	} else 
+	{ 
 		total_len = data_len;
 		extra_len = 0; 
 	}
 	
 	uint8_t input[total_len];
 	uint8_t output[sizeof(input)];
-	for (int g = 0; g < data_len; g++){   
+	for (int g = 0; g < data_len; g++)
+	{   
 		input[g] = (uint8_t)p_input[g];
 	}
-	for (int l = data_len; l < total_len; l++){  //fill with zeros couse the input must be multiple of 16
+	for (int l = data_len; l < total_len; l++)  //fill with zeros couse the input must be multiple of 16
+	{
 		input[l] = 0;
 	}
 
@@ -229,17 +250,19 @@ PoolByteArray cripter::encrypt_byte_CBC(const PoolByteArray p_input, const Strin
 	mbedtls_aes_context ctx;
 	mbedtls_aes_init( &ctx );
 	
-	if( (err = mbedtls_aes_setkey_enc( &ctx, key, 256 ) ) != 0)
+	_err = mbedtls_aes_setkey_enc( &ctx, key, 256 );
+	if( _err != 0)
 	{
-	    mbedtls_strerror( err, erro, sizeof(erro) );
+		mbedtls_strerror( _err, erro, sizeof(erro) );
 		print_error( erro );
-    }
-        
-	if( (err = mbedtls_aes_crypt_cbc( &ctx, MBEDTLS_AES_ENCRYPT, total_len, iv, input, output ) ) != 0)
+	}
+	_err = mbedtls_aes_crypt_cbc( &ctx, MBEDTLS_AES_ENCRYPT, total_len, iv, input, output );
+	if( _err != 0)
 	{
-	    mbedtls_strerror( err, erro, sizeof(erro) );
+		mbedtls_strerror( _err, erro, sizeof(erro) );
 		print_error( erro );
-    }
+	}
+	
 	mbedtls_aes_free( &ctx );
 
 
@@ -251,15 +274,19 @@ PoolByteArray cripter::encrypt_byte_CBC(const PoolByteArray p_input, const Strin
 } 
     
 PoolByteArray cripter::decrypt_byte_CBC(const PoolByteArray p_input, const String p_key) const {
-
+	
+	int _err;
+	
 	//Prepare key & iv **
 	String h_key = p_key.md5_text();
 	uint8_t key[KEY_SIZE];
 	uint8_t iv[EXT_SIZE];
-	for (int i = 0; i < KEY_SIZE; i++) {
+	for (int i = 0; i < KEY_SIZE; i++) 
+	{
 		key[i] = h_key[i];
 	}
-	for (int i = 0; i < EXT_SIZE; i++){
+	for (int i = 0; i < EXT_SIZE; i++)
+	{
 		iv[i] = key[i*2];	
 	}
 
@@ -268,10 +295,10 @@ PoolByteArray cripter::decrypt_byte_CBC(const PoolByteArray p_input, const Strin
 	int zeros = p_input[data_len];
 	uint8_t input[data_len];	
 	uint8_t output[data_len];
-	int err = 0;
-    char erro[150];
+	char erro[150];
 			
-	for (int g = 0; g < data_len; g++){   
+	for (int g = 0; g < data_len; g++)
+	{   
 		input[g] = (uint8_t)p_input[g];
 	}
 	
@@ -279,15 +306,17 @@ PoolByteArray cripter::decrypt_byte_CBC(const PoolByteArray p_input, const Strin
 	mbedtls_aes_context ctx;
 	mbedtls_aes_init( &ctx );
 	
-	if( (err = mbedtls_aes_setkey_dec(&ctx, key, 256) ) != 0)
+	_err = mbedtls_aes_setkey_dec(&ctx, key, 256);
+	if(_err != 0)
 	{
-	    mbedtls_strerror( err, erro, sizeof(erro) );
+		mbedtls_strerror( _err, erro, sizeof(erro) );
 		print_error( erro );
-    }
+	}
 	
-	if( (err = mbedtls_aes_crypt_cbc( &ctx, MBEDTLS_AES_DECRYPT, data_len, iv, input, output) ) != 0)
+	_err = mbedtls_aes_crypt_cbc( &ctx, MBEDTLS_AES_DECRYPT, data_len, iv, input, output);
+	if( _err != 0)
 	{
-	    mbedtls_strerror( err, erro, sizeof(erro) );
+	    mbedtls_strerror( _err, erro, sizeof(erro) );
 		print_error( erro );
     }
 	
@@ -303,9 +332,12 @@ PoolByteArray cripter::decrypt_byte_CBC(const PoolByteArray p_input, const Strin
 
 PoolByteArray cripter::encrypt_byte_RSA(const PoolByteArray p_input, String p_key_path) const {
 
+	int _err;
+	
 	//--- Load key
 	char key[p_key_path.length()+1];
-	for (int i = 0; i < p_key_path.length(); i++) {
+	for (int i = 0; i < p_key_path.length(); i++) 
+	{
 		key[i] = p_key_path[i];
 	}
 	key[p_key_path.length()] = 0;
@@ -314,14 +346,14 @@ PoolByteArray cripter::encrypt_byte_RSA(const PoolByteArray p_input, String p_ke
 	//---Buffer
 	size_t olen = 0;
 	const char *pers = "rsa_encrypt";
-    int err = 0;
-    char erro[150];
+	char erro[150];
     
 	uint8_t input[p_input.size()];
 	uint8_t output[512];
 	
 	PoolVector<uint8_t>::Read r = p_input.read();   
-	for (int i = 0; i < sizeof(input); i++) {
+	for (int i = 0; i < sizeof(input); i++) 
+	{
 		input[i] = (uint8_t)p_input[i];
 	}
 	
@@ -335,31 +367,38 @@ PoolByteArray cripter::encrypt_byte_RSA(const PoolByteArray p_input, String p_ke
 	
 	mbedtls_ctr_drbg_context ctr_drbg;
 	mbedtls_ctr_drbg_init( &ctr_drbg );
-        
-	if( (err = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func,
+
+
+	_err = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func,
 									&entropy, (const unsigned char *) pers,
-									strlen( pers )) ) != 0)
+									strlen( pers )) ;
+									
+	if( _err != 0)
 	{
-	    mbedtls_strerror( err, erro, sizeof(erro) );
+	    mbedtls_strerror( _err, erro, sizeof(erro) );
 		print_error( erro );
-    }
+	}
     
     
-	//---Encryptation 
-	if( ( err = mbedtls_pk_parse_public_keyfile( &pk,  key) ) != 0 )
-    {
-	    mbedtls_strerror( err, erro, sizeof(erro) );
+	//---Encryptation
+	_err = mbedtls_pk_parse_public_keyfile( &pk,  key);
+	if( _err != 0 )
+	{
+	    mbedtls_strerror( _err, erro, sizeof(erro) );
 		print_error( erro );
 	//	printf( "%c", erro );
-    }
+	}
 
 	fflush( stdout );
 
-	if( ( err = mbedtls_pk_encrypt( &pk, input, sizeof(input),
+
+	_err = mbedtls_pk_encrypt( &pk, input, sizeof(input),
 									output, &olen, sizeof(output),
-									mbedtls_ctr_drbg_random, &ctr_drbg ) ) != 0 )
+									mbedtls_ctr_drbg_random, &ctr_drbg );
+									
+	if( _err != 0 )
 	{
-	    mbedtls_strerror( err, erro, sizeof(erro) );
+	    mbedtls_strerror( _err, erro, sizeof(erro) );
 		print_error( erro );
 	}        
 
@@ -367,7 +406,7 @@ PoolByteArray cripter::encrypt_byte_RSA(const PoolByteArray p_input, String p_ke
 	//--- Fit data
 	mbedtls_pk_free( &pk);
 	mbedtls_ctr_drbg_free( &ctr_drbg );
-    mbedtls_entropy_free( &entropy );
+	mbedtls_entropy_free( &entropy );
     
 	return char2pool(output, olen);  
 	
@@ -376,17 +415,21 @@ PoolByteArray cripter::encrypt_byte_RSA(const PoolByteArray p_input, String p_ke
 
 
 PoolByteArray cripter::decrypt_byte_RSA(const PoolByteArray p_input, const String p_key_path, const String p_password) const {
-	
+
+	int _err;
+		
 	//--- Load key
 	char key[p_key_path.length()+1];
-	for (int i = 0; i < p_key_path.length(); i++) {
+	for (int i = 0; i < p_key_path.length(); i++) 
+	{
 		key[i] = p_key_path[i];
 	}
 	key[p_key_path.length()] = 0;
 
 	//--- Load Password
 	char password[p_password.length()+1];
-	for (int i = 0; i < p_password.length(); i++) {
+	for (int i = 0; i < p_password.length(); i++) 
+	{
 		password[i] = p_password[i];
 	}
 	password[p_password.length()] = 0;
@@ -395,11 +438,9 @@ PoolByteArray cripter::decrypt_byte_RSA(const PoolByteArray p_input, const Strin
 	uint8_t input[512];
 	uint8_t output[512];
 	
-    size_t olen = 0;
+	size_t olen = 0;
 	const char *pers = "rsa_decrypt";
-	    
-	int err = 0;
-    char erro[150];	
+	char erro[150];	
 	
 	PoolVector<uint8_t>::Read r = p_input.read();   
 	for (int i = 0; i < 512; i++) {
@@ -415,36 +456,42 @@ PoolByteArray cripter::decrypt_byte_RSA(const PoolByteArray p_input, const Strin
 	
 	mbedtls_ctr_drbg_context ctr_drbg;
 	mbedtls_ctr_drbg_init( &ctr_drbg );
-        
-	if( (err = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func,
+
+	_err = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func,
 							&entropy, (const unsigned char *) pers,
-							strlen( pers )) ) != 0)
+							strlen( pers ));
+
+	if(_err != 0)
 	{
-	    mbedtls_strerror( err, erro, sizeof(erro) );
+		mbedtls_strerror( _err, erro, sizeof(erro) );
 		print_error( erro );
-    }
+	}
 
 	//---Decryptation **
-	if( ( err = mbedtls_pk_parse_keyfile( &ctx_pk, key, password ) ) != 0 )
-    {
-	    mbedtls_strerror( err, erro, sizeof(erro) );
+	_err = mbedtls_pk_parse_keyfile( &ctx_pk, key, password );
+	if( _err != 0 )
+	{
+		mbedtls_strerror( _err, erro, sizeof(erro) );
 		print_error( erro );
-    }
+	}
 
 	fflush( stdout );
 
-	if( ( err = mbedtls_pk_decrypt( &ctx_pk, input, sizeof(input),
+
+	_err = mbedtls_pk_decrypt( &ctx_pk, input, sizeof(input),
 									output, &olen, sizeof(output),
-									mbedtls_ctr_drbg_random, &ctr_drbg ) ) != 0 )
+									mbedtls_ctr_drbg_random, &ctr_drbg );
+
+	if(_err != 0 )
 	{
-	    mbedtls_strerror( err, erro, sizeof(erro) );
+	    mbedtls_strerror( _err, erro, sizeof(erro) );
 		print_error( erro );
 	}        
 
 	//---Turn off the lights **
 	mbedtls_pk_free( &ctx_pk);
 	mbedtls_ctr_drbg_free( &ctr_drbg );
-    mbedtls_entropy_free( &entropy );
+	mbedtls_entropy_free( &entropy );
     
 	return char2pool(output, olen); 
 	
@@ -453,12 +500,12 @@ PoolByteArray cripter::decrypt_byte_RSA(const PoolByteArray p_input, const Strin
 
 //-------------- Support
 
-PoolByteArray cripter::char2pool(const uint8_t *p_in, const size_t p_size)const 
-{
-    PoolByteArray data;
+PoolByteArray cripter::char2pool(const uint8_t *p_in, const size_t p_size)const{
+	PoolByteArray data;
 	data.resize(p_size);
 	PoolVector<uint8_t>::Write w = data.write();
-	for (int i = 0; i < p_size; i++) {
+	for (int i = 0; i < p_size; i++) 
+	{
 		w[i] = (uint8_t)p_in[i];
 	}
 	w = PoolVector<uint8_t>::Write();
@@ -466,13 +513,13 @@ PoolByteArray cripter::char2pool(const uint8_t *p_in, const size_t p_size)const
 }
 
 
-PoolByteArray cripter::encode_var(const Variant data) const 
-{
+PoolByteArray cripter::encode_var(const Variant data) const{
 	//Encoder	
 	PoolByteArray ret;
 	int len;
 	Error err = encode_variant(data, NULL, len);
-	if (err != OK) {
+	if (err != OK) 
+	{
 		print_line("Unexpected error encoding variable to bytes");
 		return ret;
 	}
@@ -485,15 +532,15 @@ PoolByteArray cripter::encode_var(const Variant data) const
 }
 
 
-Variant cripter::decode_var(const PoolByteArray p_data) const 
-{
+Variant cripter::decode_var(const PoolByteArray p_data) const{
 	//Decoder
 	Variant ret;
 	PoolByteArray data = p_data;
 	PoolByteArray::Read r = data.read();	
 	Error err = decode_variant(ret, r.ptr(), data.size(), NULL);	
 	
-	if (err != OK) {
+	if (err != OK) 
+	{
 		print_line("Unexpected error decoding bytes to variable");
 		Variant f;
 		return f;	
