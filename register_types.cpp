3578a767dc28d646237fc56eccf6fa6e56cc888a
Cleber Alves <malkverbena@gmail.com>
2020-05-02 19:46:09 -0300

    Delete register_types.cpp

--- register_types.cpp
+++
@@ -1,8 +0,0 @@
-/* register_types.cpp */
-
-#include "register_types.h"
-#include "cripter.h"
-
-void register_cripter_types() { ClassDB::register_class<cripter>(); }
-void unregister_cripter_types() {}
-
