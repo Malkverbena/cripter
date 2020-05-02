9c24c8f680f5a0c2a2b217e4126be3c0e61a965e
Cleber Alves <malkverbena@gmail.com>
2020-05-02 18:52:40 -0300

    Add files via upload

---
+++ register_types.cpp
@@ -0,0 +1,8 @@
+/* register_types.cpp */
+
+#include "register_types.h"
+#include "cripter.h"
+
+void register_cripter_types() { ClassDB::register_class<cripter>(); }
+void unregister_cripter_types() {}
+
---
+++ register_types.h
@@ -0,0 +1,5 @@
+/* register_types.h */
+
+void register_cripter_types();
+void unregister_cripter_types();
+
