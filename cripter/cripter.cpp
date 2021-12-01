c9f447bc1b6b26d6dc2a1e728bd79ba2cef43ebd
Cleber Alves <malkverbena@gmail.com>
2021-12-01 19:27:56 -0300

    Add files via upload
    Undo

--- cripter/cripter.cpp
+++ cripter/cripter.cpp
@@ -1,7 +1,7 @@
 /*cripter.cpp*/
 
 #include "cripter.h"
-
+#include "core/print_string.h"
 
 //---Do:
 	//RSA  ---> Check if key file is valid / Maximun input size / Erros
@@ -12,23 +12,23 @@
 
 
 //-------------- Encrypt Vars
-PackedByteArray cripter::encrypt_var_CBC(const Variant p_input, const String p_key) const { return encrypt_byte_CBC((encode_var(p_input)), p_key); }
+PoolByteArray cripter::encrypt_var_CBC(const Variant p_input, const String p_key) const { return encrypt_byte_CBC((encode_var(p_input)), p_key); }
 
-PackedByteArray cripter::encrypt_var_GCM(const Variant p_input, const String p_key, const String p_add) const { return encrypt_byte_GCM((encode_var(p_input)), p_key, p_add); }
+PoolByteArray cripter::encrypt_var_GCM(const Variant p_input, const String p_key, const String p_add) const { return encrypt_byte_GCM((encode_var(p_input)), p_key, p_add); }
 
-PackedByteArray cripter::encrypt_var_RSA(const Variant p_input, const String p_key_path) const{ return encrypt_byte_RSA((encode_var(p_input)), p_key_path); }
+PoolByteArray cripter::encrypt_var_RSA(const Variant p_input, const String p_key_path) const{ return encrypt_byte_RSA((encode_var(p_input)), p_key_path); }
 
 
 //-------------- Decrypt Vars
-Variant cripter::decrypt_var_CBC(const PackedByteArray p_input, const String p_key) const{ return decode_var((decrypt_byte_CBC(p_input, p_key))); }
+Variant cripter::decrypt_var_CBC(const PoolByteArray p_input, const String p_key) const{ return decode_var((decrypt_byte_CBC(p_input, p_key))); }
 
-Variant cripter::decrypt_var_GCM(const PackedByteArray p_input, const String p_key, const String p_add) const { return decode_var(decrypt_byte_GCM(p_input, p_key, p_add)); }
+Variant cripter::decrypt_var_GCM(const PoolByteArray p_input, const String p_key, const String p_add) const { return decode_var(decrypt_byte_GCM(p_input, p_key, p_add)); }
 
-Variant cripter::decrypt_var_RSA(const PackedByteArray p_input, const String p_key_path, const String p_password) const{ return decode_var((decrypt_byte_RSA(p_input, p_key_path, p_password))); }
+Variant cripter::decrypt_var_RSA(const PoolByteArray p_input, const String p_key_path, const String p_password) const{ return decode_var((decrypt_byte_RSA(p_input, p_key_path, p_password))); }
 
 
 //-------------- Simetric - GCM
-PackedByteArray cripter::encrypt_byte_GCM(const PackedByteArray p_input, const String p_key, const String p_add) const {
+PoolByteArray cripter::encrypt_byte_GCM(const PoolByteArray p_input, const String p_key, const String p_add) const {
 	int _err = 0;
 	//Prepare key & iv **
 	String h_key = p_key.md5_text();
@@ -45,7 +45,7 @@ PackedByteArray cripter::encrypt_byte_GCM(const PackedByteArray p_input, const S
 	uint8_t input[p_input.size()];
 	uint8_t output[sizeof(input)];
 
-	Vector<uint8_t> r = p_input;   //PackedByteArray to CharArray
+	PoolVector<uint8_t>::Read r = p_input.read();   //PoolByteArray to CharArray
 	for (int i = 0; i < p_input.size(); i++) {
 		input[i] = (uint8_t)p_input[i];
 	}
@@ -80,14 +80,14 @@ PackedByteArray cripter::encrypt_byte_GCM(const PackedByteArray p_input, const S
 	}
 
 	mbedtls_gcm_free( &ctx );
-	PackedByteArray ret_output = char2pool(output, sizeof(output));
-	PackedByteArray ret_tag = char2pool(tag, sizeof(tag));
+	PoolByteArray ret_output = char2pool(output, sizeof(output));
+	PoolByteArray ret_tag = char2pool(tag, sizeof(tag));
 	ret_output.append_array(ret_tag);
 	return ret_output;
 }
 
 
-PackedByteArray cripter::decrypt_byte_GCM(const PackedByteArray p_input, const String p_key, const String p_add) const{
+PoolByteArray cripter::decrypt_byte_GCM(const PoolByteArray p_input, const String p_key, const String p_add) const{
 	int _err;
 	//Prepare key & iv **
 	String h_key = p_key.md5_text();
@@ -102,18 +102,18 @@ PackedByteArray cripter::decrypt_byte_GCM(const PackedByteArray p_input, const S
 
 	//Preparing Buffer
 	char erro[150];
-	PackedByteArray ret_output;
+	PoolByteArray ret_output;
 	int data_len = p_input.size();
 	uint8_t input[(data_len - TAG_SIZE)];
 	uint8_t output[sizeof(input)];
-	Vector<uint8_t> r = p_input;
+	PoolVector<uint8_t>::Read r = p_input.read();
 	for (int i = 0; i < (data_len - TAG_SIZE); i++) {
 		input[i] = (uint8_t)p_input[i];
 	}
 
 	//Extract Tag
 	uint8_t tag[TAG_SIZE];
-	Vector<uint8_t> R = p_input;
+	PoolVector<uint8_t>::Read R = p_input.read();
 	for (int i = 0; i < TAG_SIZE; i++) {
 		tag[i] = (uint8_t)p_input[ (data_len - TAG_SIZE) + i];
 	}
@@ -151,7 +151,7 @@ PackedByteArray cripter::decrypt_byte_GCM(const PackedByteArray p_input, const S
 
 
 //-------------- Simetric - CBC
-PackedByteArray cripter::encrypt_byte_CBC(const PackedByteArray p_input, const String p_key) const{
+PoolByteArray cripter::encrypt_byte_CBC(const PoolByteArray p_input, const String p_key) const{
 	int _err;
 	//Prepare key & iv **
 	String h_key = p_key.md5_text();
@@ -204,13 +204,13 @@ PackedByteArray cripter::encrypt_byte_CBC(const PackedByteArray p_input, const S
 
 	mbedtls_aes_free( &ctx );
 	//--- Fit data *
-	PackedByteArray ret = char2pool(output, (sizeof(output)));
+	PoolByteArray ret = char2pool(output, (sizeof(output)));
 	ret.push_back(extra_len);
 	return ret;
 }
 
 
-PackedByteArray cripter::decrypt_byte_CBC(const PackedByteArray p_input, const String p_key) const {
+PoolByteArray cripter::decrypt_byte_CBC(const PoolByteArray p_input, const String p_key) const {
 	int _err;
 	//Prepare key & iv **
 	String h_key = p_key.md5_text();
@@ -253,7 +253,7 @@ PackedByteArray cripter::decrypt_byte_CBC(const PackedByteArray p_input, const S
 
 
 //-------------- Asymmetric - RSA
-PackedByteArray cripter::encrypt_byte_RSA(const PackedByteArray p_input, String p_key_path) const {
+PoolByteArray cripter::encrypt_byte_RSA(const PoolByteArray p_input, String p_key_path) const {
 	int _err;
 	//--- Load key
 	char key[p_key_path.length()+1];
@@ -267,7 +267,7 @@ PackedByteArray cripter::encrypt_byte_RSA(const PackedByteArray p_input, String 
 	char erro[150];
 	uint8_t input[p_input.size()];
 	uint8_t output[512];
-	Vector<uint8_t> r = p_input;
+	PoolVector<uint8_t>::Read r = p_input.read();
 	for (unsigned int i = 0; i < sizeof(input); i++) {
 		input[i] = (uint8_t)p_input[i];
 	}
@@ -312,7 +312,7 @@ PackedByteArray cripter::encrypt_byte_RSA(const PackedByteArray p_input, String 
 }
 
 
-PackedByteArray cripter::decrypt_byte_RSA(const PackedByteArray p_input, const String p_key_path, const String p_password) const {
+PoolByteArray cripter::decrypt_byte_RSA(const PoolByteArray p_input, const String p_key_path, const String p_password) const {
 	int _err;
 	//--- Load key
 	char key[p_key_path.length()+1];
@@ -333,7 +333,7 @@ PackedByteArray cripter::decrypt_byte_RSA(const PackedByteArray p_input, const S
 	size_t olen = 0;
 	const char *pers = "rsa_decrypt";
 	char erro[150];
-	Vector<uint8_t> r = p_input;
+	PoolVector<uint8_t>::Read r = p_input.read();
 	for (int i = 0; i < 512; i++) {
 		input[i] = (uint8_t)p_input[i];
 	}
@@ -377,19 +377,21 @@ PackedByteArray cripter::decrypt_byte_RSA(const PackedByteArray p_input, const S
 
 
 //-------------- Support
-PackedByteArray cripter::char2pool(const uint8_t *p_in, const size_t p_size)const{
-	PackedByteArray data;
+PoolByteArray cripter::char2pool(const uint8_t *p_in, const size_t p_size)const{
+	PoolByteArray data;
 	data.resize(p_size);
+	PoolVector<uint8_t>::Write w = data.write();
 	for (unsigned int i = 0; i < p_size; i++) {
-		data.push_back((uint8_t)p_in[i]);
+		w[i] = (uint8_t)p_in[i];
 	}
+	w = PoolVector<uint8_t>::Write();
 	return data;
 }
 
 
-PackedByteArray cripter::encode_var(const Variant data) const{
+PoolByteArray cripter::encode_var(const Variant data) const{
 	//Encoder
-	PackedByteArray ret;
+	PoolByteArray ret;
 	int len;
 	Error err = encode_variant(data, NULL, len);
 	if (err != OK) {
@@ -398,18 +400,18 @@ PackedByteArray cripter::encode_var(const Variant data) const{
 	}
 	ret.resize(len);
 	{
-		PackedByteArray w = ret;
-		encode_variant(data, w.ptrw(), len);
+		PoolByteArray::Write w = ret.write();
+		encode_variant(data, w.ptr(), len);
 	}
 	return ret;
 }
 
 
-Variant cripter::decode_var(const PackedByteArray p_data) const{
+Variant cripter::decode_var(const PoolByteArray p_data) const{
 	//Decoder
 	Variant ret;
-	PackedByteArray data = p_data;
-	PackedByteArray r = data;
+	PoolByteArray data = p_data;
+	PoolByteArray::Read r = data.read();
 	Error err = decode_variant(ret, r.ptr(), data.size(), NULL);
 	if (err != OK) {
 		print_line("Unexpected error decoding bytes to variable");
--- cripter/cripter.h
+++ cripter/cripter.h
@@ -2,10 +2,8 @@
 #ifndef CRIPTER_H
 #define CRIPTER_H
 
+#include "core/reference.h"
 #include "core/io/marshalls.h"
-#include "core/object/ref_counted.h"
-#include "core/string/print_string.h"
-#include "thirdparty/mbedtls/include/mbedtls/error.h"
 
 #include "thirdparty/mbedtls/include/mbedtls/gcm.h"
 #include "thirdparty/mbedtls/include/mbedtls/aes.h"
@@ -22,38 +20,39 @@
 #include "thirdparty/mbedtls/include/mbedtls/bignum.h"
 */
 
-
 #pragma once
 
 
-class cripter : public RefCounted{
-	GDCLASS(cripter,RefCounted);
+class cripter : public Reference {
+	GDCLASS(cripter,Reference);
 
 
 private:
-	PackedByteArray encode_var(const Variant p_data) const;
-	Variant decode_var(const PackedByteArray p_data) const;
-	PackedByteArray char2pool(const uint8_t *p_in, const size_t p_size) const;
+	PoolByteArray encode_var(const Variant p_data) const;
+	Variant decode_var(const PoolByteArray p_data) const;
+	PoolByteArray char2pool(const uint8_t *p_in, const size_t p_size) const;
+
 
 protected:
 	static void _bind_methods();
 
+
 public:
 	//CBC
-	PackedByteArray encrypt_byte_CBC(const PackedByteArray p_input, const String p_key) const;
-	PackedByteArray decrypt_byte_CBC(const PackedByteArray p_input, const String p_key) const;
-	PackedByteArray encrypt_var_CBC(const Variant p_input, const String p_key) const;
-	Variant decrypt_var_CBC(const PackedByteArray p_input, const String p_key) const;
+	PoolByteArray encrypt_byte_CBC(const PoolByteArray p_input, const String p_key) const;
+	PoolByteArray decrypt_byte_CBC(const PoolByteArray p_input, const String p_key) const;
+	PoolByteArray encrypt_var_CBC(const Variant p_input, const String p_key) const;
+	Variant decrypt_var_CBC(const PoolByteArray p_input, const String p_key) const;
 	//GCM
-	PackedByteArray encrypt_byte_GCM(const PackedByteArray p_input, const String p_key, const String p_add = "") const;
-	PackedByteArray decrypt_byte_GCM(const PackedByteArray p_input, const String p_key, const String p_add = "") const;
-	PackedByteArray encrypt_var_GCM(const Variant p_input, const String p_key, const String p_add = "") const;
-	Variant decrypt_var_GCM(const PackedByteArray p_input, const String p_key, const String p_add = "") const;
+	PoolByteArray encrypt_byte_GCM(const PoolByteArray p_input, const String p_key, const String p_add = "") const;
+	PoolByteArray decrypt_byte_GCM(const PoolByteArray p_input, const String p_key, const String p_add = "") const;
+	PoolByteArray encrypt_var_GCM(const Variant p_input, const String p_key, const String p_add = "") const;
+	Variant decrypt_var_GCM(const PoolByteArray p_input, const String p_key, const String p_add = "") const;
 	//RSA
-	PackedByteArray encrypt_byte_RSA(const PackedByteArray p_input,  String p_key_path) const;
-	PackedByteArray decrypt_byte_RSA(const PackedByteArray p_input, const String p_key_path, const String p_password) const;
-	PackedByteArray encrypt_var_RSA(const Variant p_input, const String p_key_path) const;
-	Variant decrypt_var_RSA(const PackedByteArray p_input, const String p_key_path, const String p_password) const;
+	PoolByteArray encrypt_byte_RSA(const PoolByteArray p_input,  String p_key_path) const;
+	PoolByteArray decrypt_byte_RSA(const PoolByteArray p_input, const String p_key_path, const String p_password) const;
+	PoolByteArray encrypt_var_RSA(const Variant p_input, const String p_key_path) const;
+	Variant decrypt_var_RSA(const PoolByteArray p_input, const String p_key_path, const String p_password) const;
 
 	cripter();
 };
