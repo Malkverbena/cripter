b8f6eb8f9ca9119cfc0f00755261c733b316eecc
Cleber Alves <malkverbena@gmail.com>
2021-12-01 19:28:43 -0300

    Add files via upload
    Update to Godot 4

--- cripter/cripter.h
+++ cripter/cripter.h
@@ -2,8 +2,10 @@
 #ifndef CRIPTER_H
 #define CRIPTER_H
 
-#include "core/reference.h"
 #include "core/io/marshalls.h"
+#include "core/object/ref_counted.h"
+#include "core/string/print_string.h"
+#include "thirdparty/mbedtls/include/mbedtls/error.h"
 
 #include "thirdparty/mbedtls/include/mbedtls/gcm.h"
 #include "thirdparty/mbedtls/include/mbedtls/aes.h"
@@ -20,39 +22,38 @@
 #include "thirdparty/mbedtls/include/mbedtls/bignum.h"
 */
 
+
 #pragma once
 
 
-class cripter : public Reference {
-	GDCLASS(cripter,Reference);
+class cripter : public RefCounted{
+	GDCLASS(cripter,RefCounted);
 
 
 private:
-	PoolByteArray encode_var(const Variant p_data) const;
-	Variant decode_var(const PoolByteArray p_data) const;
-	PoolByteArray char2pool(const uint8_t *p_in, const size_t p_size) const;
-
+	PackedByteArray encode_var(const Variant p_data) const;
+	Variant decode_var(const PackedByteArray p_data) const;
+	PackedByteArray char2pool(const uint8_t *p_in, const size_t p_size) const;
 
 protected:
 	static void _bind_methods();
 
-
 public:
 	//CBC
-	PoolByteArray encrypt_byte_CBC(const PoolByteArray p_input, const String p_key) const;
-	PoolByteArray decrypt_byte_CBC(const PoolByteArray p_input, const String p_key) const;
-	PoolByteArray encrypt_var_CBC(const Variant p_input, const String p_key) const;
-	Variant decrypt_var_CBC(const PoolByteArray p_input, const String p_key) const;
+	PackedByteArray encrypt_byte_CBC(const PackedByteArray p_input, const String p_key) const;
+	PackedByteArray decrypt_byte_CBC(const PackedByteArray p_input, const String p_key) const;
+	PackedByteArray encrypt_var_CBC(const Variant p_input, const String p_key) const;
+	Variant decrypt_var_CBC(const PackedByteArray p_input, const String p_key) const;
 	//GCM
-	PoolByteArray encrypt_byte_GCM(const PoolByteArray p_input, const String p_key, const String p_add = "") const;
-	PoolByteArray decrypt_byte_GCM(const PoolByteArray p_input, const String p_key, const String p_add = "") const;
-	PoolByteArray encrypt_var_GCM(const Variant p_input, const String p_key, const String p_add = "") const;
-	Variant decrypt_var_GCM(const PoolByteArray p_input, const String p_key, const String p_add = "") const;
+	PackedByteArray encrypt_byte_GCM(const PackedByteArray p_input, const String p_key, const String p_add = "") const;
+	PackedByteArray decrypt_byte_GCM(const PackedByteArray p_input, const String p_key, const String p_add = "") const;
+	PackedByteArray encrypt_var_GCM(const Variant p_input, const String p_key, const String p_add = "") const;
+	Variant decrypt_var_GCM(const PackedByteArray p_input, const String p_key, const String p_add = "") const;
 	//RSA
-	PoolByteArray encrypt_byte_RSA(const PoolByteArray p_input,  String p_key_path) const;
-	PoolByteArray decrypt_byte_RSA(const PoolByteArray p_input, const String p_key_path, const String p_password) const;
-	PoolByteArray encrypt_var_RSA(const Variant p_input, const String p_key_path) const;
-	Variant decrypt_var_RSA(const PoolByteArray p_input, const String p_key_path, const String p_password) const;
+	PackedByteArray encrypt_byte_RSA(const PackedByteArray p_input,  String p_key_path) const;
+	PackedByteArray decrypt_byte_RSA(const PackedByteArray p_input, const String p_key_path, const String p_password) const;
+	PackedByteArray encrypt_var_RSA(const Variant p_input, const String p_key_path) const;
+	Variant decrypt_var_RSA(const PackedByteArray p_input, const String p_key_path, const String p_password) const;
 
 	cripter();
 };
