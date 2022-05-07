7bf9f27cdc038782a2484ec9478e325bbbf20265
Cleber Alves <malkverbena@gmail.com>
2022-05-07 19:10:03 -0300

    Add files via upload

--- cripter/register_types.cpp
+++ cripter/register_types.cpp
@@ -3,12 +3,31 @@
 #include "register_types.h"
 #include "cripter.h"
 	 
+
+#ifdef GD4
+
+void initialize_cripter_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
+	GDREGISTER_CLASS(Cripter);
+}
+
+void uninitialize_cripter_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
+}
+
+#else
+
 void register_cripter_types() {
 	ClassDB::register_class<Cripter>();
 }
 
-
 void unregister_cripter_types() {
 }
+
+#endif
 
 
--- cripter/register_types.h
+++ cripter/register_types.h
@@ -1,5 +1,29 @@
 /* register_types.h */
 
+
+#ifndef CRIPTER_REGISTER_TYPES_H
+#define CRIPTER_REGISTER_TYPES_H
+
+#include "modules/register_module_types.h"
+
+#ifndef GD4
+
+void initialize_cripter_module(ModuleInitializationLevel p_level);
+void uninitialize_cripter_module(ModuleInitializationLevel p_level);
+
+#else
+
 void register_cripter_types();
 void unregister_cripter_types();
+
+#endif
+
+
+
+#endif // CRIPTER_REGISTER_TYPES_H
+
+
+
+
+
 
