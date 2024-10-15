87586c567370192695fe5769d1c5ff0729fe5916
Cleber <malkverbena@gmail.com>
2024-10-15 11:10:08 -0300

    Add basic GCM encryption functions.

--- cripter.cpp
+++ cripter.cpp
@@ -4,6 +4,117 @@
 
 
 
+// =============== GCM FUNCTION =============== 
+
+
+
+Dictionary Cripter::gcm_encrypt(
+	const PackedByteArray &plaintext,
+	const String &p_password,
+	const PackedByteArray &iv,
+	const String &p_aad,
+	Cripter::KeySize p_keybits
+	){
+
+	if (p_keybits < BITS_128){
+		WARN_PRINT("GCM Algorithm only accepts key with 128, 192, or 256 bits");
+		p_keybits = Cripter::BITS_128;
+	}
+	if (p_keybits > BITS_256){
+		WARN_PRINT("GCM Algorithm only accepts key with 128, 192, or 256 bits");
+		p_keybits = Cripter::BITS_256;
+	}
+
+	mbedtls_gcm_context gcm_ctx;
+	mbedtls_gcm_init(&gcm_ctx);
+	unsigned char tag[16];
+	unsigned char * password = (unsigned char *)(p_password.utf8().get_data());
+	unsigned char * aad = (unsigned char *)(p_aad.utf8().get_data());
+	size_t aad_len = p_aad.size();
+	size_t plaintext_len = plaintext.size();
+	PackedByteArray output;
+	output.resize(plaintext_len);
+
+	int mbedtls_erro = mbedtls_gcm_setkey(&gcm_ctx, MBEDTLS_CIPHER_ID_AES, password, p_keybits);
+	if (mbedtls_erro != OK) {
+		mbedtls_gcm_free(&gcm_ctx);
+		String _err = mbed_error_msn(mbedtls_erro, "mbedtls_gcm_setkey");
+		ERR_FAIL_V_EDMSG(Dictionary(), String("Failed to configure GCM key.: -0x") + String::num_int64(-mbedtls_erro, 16) + _err);
+	}
+
+	mbedtls_erro = mbedtls_gcm_crypt_and_tag(
+		&gcm_ctx, MBEDTLS_GCM_ENCRYPT, plaintext_len,
+		iv.ptr(), iv.size(),
+		aad, aad_len,
+		plaintext.ptr(), output.ptrw(),
+		sizeof(tag), tag
+	);
+
+	mbedtls_gcm_free(&gcm_ctx);
+
+	if (mbedtls_erro != OK) {
+		String _err = mbed_error_msn(mbedtls_erro, "mbedtls_gcm_crypt_and_tag");
+		ERR_FAIL_V_EDMSG(Dictionary(), String("Encryption error. : -0x") + String::num_int64(-mbedtls_erro, 16) + _err);
+	}
+
+	String str_tag = String((const char *)tag);
+	Dictionary ret;
+	ret["Tag"] = str_tag;
+	ret["Ciphertext"] = output;
+	return ret;
+}
+
+
+Dictionary Cripter::gcm_decrypt(
+	const PackedByteArray &ciphertext,
+	const String &p_password,
+	const PackedByteArray &iv,
+	const String &p_aad,
+	const PackedByteArray &p_tag,
+	Cripter::KeySize p_keybits
+	) {
+
+	if (p_keybits < BITS_128) {
+		WARN_PRINT("GCM Algorithm only accepts key with 128, 192, or 256 bits");
+		p_keybits = Cripter::BITS_128;
+	}
+	if (p_keybits > BITS_256) {
+		WARN_PRINT("GCM Algorithm only accepts key with 128, 192, or 256 bits");
+		p_keybits = Cripter::BITS_256;
+	}
+
+	mbedtls_gcm_context gcm_ctx;
+	mbedtls_gcm_init(&gcm_ctx);
+
+	unsigned char *password = (unsigned char *)(p_password.utf8().get_data());
+	unsigned char *aad = (unsigned char *)(p_aad.utf8().get_data());
+	size_t aad_len = p_aad.size();
+	size_t ciphertext_len = ciphertext.size();
+	size_t tag_len = p_tag.size();
+	PackedByteArray output;
+	output.resize(ciphertext_len);
+
+	// Configuração da chave GCM
+	int mbedtls_erro = mbedtls_gcm_setkey(&gcm_ctx, MBEDTLS_CIPHER_ID_AES, password, p_keybits);
+	if (mbedtls_erro != 0) {
+		mbedtls_gcm_free(&gcm_ctx);
+		String _err = mbed_error_msn(mbedtls_erro, "mbedtls_gcm_setkey");
+		ERR_FAIL_V_EDMSG(Dictionary(), String("Failed to configure GCM key: -0x") + String::num_int64(-mbedtls_erro, 16) + _err);
+	}
+
+	mbedtls_erro = mbedtls_gcm_auth_decrypt(&gcm_ctx, ciphertext_len, iv.ptr(), iv.size(), aad, aad_len, p_tag.ptr(), tag_len, ciphertext.ptr(), output.ptrw());
+
+	mbedtls_gcm_free(&gcm_ctx);
+
+	if (mbedtls_erro != 0) {
+		String _err = mbed_error_msn(mbedtls_erro, "mbedtls_gcm_auth_decrypt");
+		ERR_FAIL_V_EDMSG(Dictionary(), String("Decryption error: -0x") + String::num_int64(-mbedtls_erro, 16) + _err);
+	}
+
+	Dictionary ret;
+	ret["Plaintext"] = output;
+	return ret;
+}
 
 
 
@@ -94,6 +205,13 @@ String Cripter::mbed_error_msn(int mbedtls_erro, const char* p_function){
 // =============== GODOT CLASS =============== 
 
 void Cripter::_bind_methods(){
+
+	ClassDB::bind_static_method("Cripter", D_METHOD("generate_iv", "iv length", "personalization"), &Cripter::generate_iv);
+	ClassDB::bind_static_method("Cripter", D_METHOD("derive_key_pbkdf2", "password", "salt", "iterations", "key_length"), &Cripter::derive_key_pbkdf2, DEFVAL(500), DEFVAL(16));
+
+	ClassDB::bind_static_method("Cripter", D_METHOD("gcm_encrypt", "plaintext", "password", "IV", "AAD", "keybits"), &Cripter::gcm_encrypt, DEFVAL(String()), DEFVAL(BITS_256));
+	ClassDB::bind_static_method("Cripter", D_METHOD("gcm_decrypt", "ciphertext", "password", "IV", "AAD", "tag", "keybits"), &Cripter::gcm_decrypt, DEFVAL(BITS_256));
+
 }
 
 
--- cripter.h
+++ cripter.h
@@ -35,12 +35,61 @@ protected:
 	static void _bind_methods();
 
 
+
+
 public:
 
-	static PackedByteArray generate_iv(const int iv_length, const String p_personalization);
 
-	static String derive_key_pbkdf2(const String p_password, const String p_salt, int iterations = 500, int key_length = 16);
+	enum KeySize { // - Standard sizes for keys.
+		BITS_128 = 128,
+		BITS_192 = 192,
+		BITS_256 = 256,
+		BITS_512 = 512,
+		BITS_1024 = 1024,
+		BITS_2048 = 2048,
+		BITS_3072 = 3072,
+		BITS_4096 = 4096,
+		BITS_7680 = 7680,
+		BITS_8192 = 8192,
+	};
 
+
+
+
+
+
+
+
+
+
+	static PackedByteArray generate_iv(
+		const int iv_length, 
+		const String p_personalization
+	);
+
+	static String derive_key_pbkdf2(
+		const String p_password, const String p_salt, 
+		int iterations = 500, 
+		int key_length = 16
+	);
+
+
+	static Dictionary gcm_encrypt(
+		const PackedByteArray &plaintext, 
+		const String &p_password, 
+		const PackedByteArray &iv, 
+		const String &p_aad = String(), 
+		Cripter::KeySize p_keybits = BITS_256
+	);
+
+	static Dictionary gcm_decrypt(
+		const PackedByteArray &ciphertext,
+		const String &p_password,
+		const PackedByteArray &iv,
+		const String &p_aad,
+		const PackedByteArray &p_tag,
+		Cripter::KeySize p_keybits = BITS_256
+	);
 
 
 	Cripter();
